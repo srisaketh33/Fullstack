e5c2587ae0aeb3773ce017f9cb6d4a6933f5a4a3
SRI TULASI SATYA SAKETH.R <117180580+srisaketh33@users.noreply.github.com>
2025-11-28 17:48:21 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-MONGO_URI="mongodb+srv://srisaketh33_db_user:18Saketh10@cluster0.xn4ppya.mongodb.net/?retryWrites=true&w=majority"
-MONGO_DB="authdb"
-JWT_SECRET="your_super_secret_key"
-JWT_ALGORITHM="HS256"
-ACCESS_TOKEN_EXPIRES_SECONDS=900
-REFRESH_TOKEN_EXPIRES_SECONDS=1209600
-REFRESH_TOKEN_ROTATION=true
